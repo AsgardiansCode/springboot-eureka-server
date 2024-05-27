623e82b34bae8c42fbe8efac18f66e09db7f6272
Chamali Vishmani <chamali106astro@gmail.com>
2024-05-27 18:28:14 +0530

    add drug list service

---
+++ api-gateway/src/main/java/com/example/springcloudgatewayoverview/model/Drug.java
@@ -0,0 +1,14 @@
+package com.example.springcloudgatewayoverview.model;
+
+public class Drug{
+    private String drugA;
+    private String drugB;
+
+    public Drug() {
+    }
+
+    public Drug(String drugA, String drugB) {
+        this.drugA = drugA;
+        this.drugB = drugB;
+    }
+}
